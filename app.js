8773e6c5555f014ae86999932011a30a121654f4
mohamadhborna <mhborna2000@gmail.com>
2023-04-30 13:58:28 +0330

    a simple connection between client and server using socket io added

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const socketio = require('socket.io')
+const http = require('http')
 
-const app = express();
+const app = express(); //Request Handler valid createserver
+const server = http.createServer(app)
+const io = new socketio.Server(server)
 
 //* static folder
 app.use(express.static('public'))
@@ -8,7 +12,17 @@ app.use(express.static('public'))
 
 const port = process.env.PORT || 4200
 
-
-app.listen(port , () =>{
-    console.log(`server is Running on port ${port}`)
+server.listen(port , () =>{
+    console.log(`server is running on port ${port}`)
 })
+
+//* socket set up ==> we need event sender and event listener
+io.on('connection' , (socket) =>{
+    console.log('Client connected' + socket.id)
+})
+
+
+
+// app.listen(port , () =>{
+//     console.log(`server is Running on port ${port}`)
+// })
